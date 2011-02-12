b6a71aaa2317d6f8a9eb2c050bd506d7595913d3
Matt Curry <mattjcurry@gmail.com>
2011-02-11 17:20:32 -0800

    Exchanged some gate objects for their aliases.

--- examples/advanced/quantum_error_correction.py
+++ examples/advanced/quantum_error_correction.py
@@ -2,7 +2,7 @@
 An example of creating a 5-qubit encoding circuit is given.
 """
 
-from sympy.physics.quantum.gate import H, Z, CNotGate
+from sympy.physics.quantum.gate import H, Z, CNOT
 from sympy.physics.quantum.qubit import Qubit
 from sympy.physics.quantum.applyops import apply_operators
 from sympy.printing.pretty import pprint
@@ -18,10 +18,10 @@ print ""
 
 print "Create encoding circuit:"
 print ""
-# H <=> HadamardGate
-# Z <=> ZGate
-qbec = H(3)*H(4)*CNotGate(2, 0)*CNotGate(3, 0)*CNotGate(4, 0)*H(1)*H(4)*CNotGate(2, 1)*\
-        CNotGate(4, 1)*H(2)*CNotGate(3, 2)*CNotGate(4, 2)*H(3)*H(4)*CNotGate(4, 3)*\
-        Z(4)*H(4)*Z(4)
+# CNOT <=> Controlled-Not Gate
+# H <=> Hadamard Gate
+# Z <=> Z Gate
+qbec = H(3)*H(4)*CNOT(2, 0)*CNOT(3, 0)*CNOT(4, 0)*H(1)*H(4)*CNOT(2, 1)*\
+        CNOT(4, 1)*H(2)*CNOT(3, 2)*CNOT(4, 2)*H(3)*H(4)*CNOT(4, 3)*Z(4)*H(4)*Z(4)
 pprint(qbec)
-alt_ec = CNotGate(4, 0)*CNotGate(4, 1)*CNotGate(4, 2)*CNotGate(4, 3)
+alt_ec = CNOT(4, 0)*CNOT(4, 1)*CNOT(4, 2)*CNOT(4, 3)
--- examples/advanced/quantum_gate_decomposition.py
+++ examples/advanced/quantum_gate_decomposition.py
@@ -2,20 +2,23 @@
 An example of decomposing multi-qubit gates into elementary gates is given.
 """
 
-from sympy.physics.quantum.gate import CGate, YGate, ZGate, SwapGate
+from sympy.physics.quantum.gate import CGate, Y, Z, SWAP
 from sympy.printing.pretty import pprint
 
 print "Create some multi-qubit gates:"
 print ""
-CY10 = CGate(1, YGate(0))
+# SWAP <=> Swap Gate
+# Y <=> Y Gate
+# Z <=> Z Gate
+CY10 = CGate(1, Y(0))
 print "Controlled-Y Gate"
 pprint(CY10)
 print ""
-CZ01 = CGate(0, ZGate(1))
+CZ01 = CGate(0, Z(1))
 print "Controlled-Z Gate"
 pprint(CZ01)
 print ""
-SWAP23 = SwapGate(2, 3)
+SWAP23 = SWAP(2, 3)
 print "SWAP Gate"
 pprint(SWAP23)
 print ""
