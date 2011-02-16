893e69656474e942b367c14e82e3c78fb043dad8
Matt Curry <mattjcurry@gmail.com>
2011-02-15 17:58:37 -0800

    Prepared code for SQuInT 2011 poster.

--- examples/advanced/quantum_error_correction.py
+++ examples/advanced/quantum_error_correction.py
@@ -1,55 +1,31 @@
 """
 An example of 5-qubit error correcting code is given.
+
+* Create codeword operators.
+* Apply these operators to states.
+* Plot encoding circuit.
 """
 
 from sympy.physics.quantum.gate import H, X, Z, CNOT
-from sympy.physics.quantum.qubit import Qubit
+from sympy.physics.quantum.qubit import IntQubit
 from sympy.physics.quantum.applyops import apply_operators
 from sympy.printing.pretty import pprint
 from sympy.physics.quantum.circuitplot import circuit_plot
 from sympy.core.numbers import Rational
 
-print "Create codeword operators:"
-print ""
-print "M0:"
-M0 = Z(1)*X(2)*X(3)*Z(4)
-pprint(M0)
-print ""
-print "M1:"
-M1 = Z(2)*X(3)*X(4)*Z(0)
-pprint(M1)
-print ""
-print "M2:"
-M2 = Z(3)*X(4)*X(0)*Z(1)
-pprint(M2)
-print ""
-print "M3:"
-M3 = Z(4)*X(0)*X(1)*Z(2)
-pprint(M3)
-print ""
+M0 = Z(1)*X(2)*X(3)*Z(4); M0
+M1 = Z(2)*X(3)*X(4)*Z(0); M1
+M2 = Z(3)*X(4)*X(0)*Z(1); M2
+M3 = Z(4)*X(0)*X(1)*Z(2); M3
 
-print "Apply these operators to create codeword states:"
-print ""
-print "Zero:"
-zero = Rational(1,4)*(1+M0)*(1+M1)*(1+M2)*(1+M3)*Qubit('00000')
-pprint(zero)
-print ""
-print "Apply the operators:"
-pprint(apply_operators(zero))
-print ""
-print "One:"
-one = Rational(1,4)*(1+M0)*(1+M1)*(1+M2)*(1+M3)*Qubit('11111')
-pprint(one)
-print ""
-print "Apply the operators:"
-pprint(apply_operators(one))
-print ""
+zero = Rational(1,4)*(1+M0)*(1+M1)*(1+M2)*(1+M3)*IntQubit(0, 5); zero
+apply_operators(zero)
+one = Rational(1,4)*(1+M0)*(1+M1)*(1+M2)*(1+M3)*IntQubit(2**5-1, 5); one
+apply_operators(one)
 
-print "Create encoding circuit:"
-print ""
 # CNOT <=> Controlled-Not Gate
 # H <=> Hadamard Gate
 # Z <=> Z Gate
-qbec = H(3)*H(4)*CNOT(2, 0)*CNOT(3, 0)*CNOT(4, 0)*H(1)*H(4)*CNOT(2, 1)*\
+encoding_circuit = H(3)*H(4)*CNOT(2, 0)*CNOT(3, 0)*CNOT(4, 0)*H(1)*H(4)*CNOT(2, 1)*\
         CNOT(4, 1)*H(2)*CNOT(3, 2)*CNOT(4, 2)*H(3)*H(4)*CNOT(4, 3)*Z(4)*H(4)*Z(4)
-circuit_plot(qbec, nqubits=5)
+circuit_plot(encoding_circuit, nqubits=5)
--- examples/advanced/quantum_gate_decomposition.py
+++ examples/advanced/quantum_gate_decomposition.py
@@ -6,34 +6,18 @@ from sympy.physics.quantum.gate import CGate, Y, Z, SWAP
 from sympy.printing.pretty import pprint
 from sympy.physics.quantum.circuitplot import circuit_plot
 
-print "Create some multi-qubit gates:"
-print ""
 # SWAP <=> Swap Gate
 # Y <=> Y Gate
 # Z <=> Z Gate
-CY10 = CGate(1, Y(0))
-print "Controlled-Y Gate"
-pprint(CY10)
-print ""
-CZ01 = CGate(0, Z(1))
-print "Controlled-Z Gate"
-pprint(CZ01)
-print ""
-SWAP23 = SWAP(2, 3)
-print "SWAP Gate"
-pprint(SWAP23)
-print ""
+CY10 = CGate(1, Y(0)); CY10
+CZ01 = CGate(0, Z(1)); CZ01
+SWAP10 = SWAP(1, 0); SWAP10
 
-print "And now their decomposition:"
-print ""
-print "Decomposed controlled-Y Gate"
-pprint(CY10.decompose())
+CY10.decompose()
 circuit_plot(CY10.decompose(), nqubits=2)
-print ""
-print "Decomposed controlled-Z Gate"
-pprint(CZ01.decompose())
+
+CZ01.decompose()
 circuit_plot(CZ01.decompose(), nqubits=2)
-print ""
-print "Decomposed SWAP Gate"
-pprint(SWAP23.decompose())
-circuit_plot(CY10.decompose(), nqubits=4)
+
+SWAP10.decompose()
+circuit_plot(SWAP10.decompose(), nqubits=2)
