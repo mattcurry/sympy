8b92ec537fc45689fd89d9b90077805035f249b7
Matt Curry <mattjcurry@gmail.com>
2011-05-04 00:43:08 -0700

    Implemented basic quantum gate rule pattern matching and added some gate rules.

--- sympy/physics/quantum/gaterules.py
+++ sympy/physics/quantum/gaterules.py
@@ -1,28 +1,52 @@
-from sympy import Basic
+from sympy import Basic, Mul
 from sympy.physics.quantum.gate import X, Y, Z, S, T, H, CNOT, SWAP
 
 class GateRule(Basic):
 
     @property
     def lhs(self):
-        return self.args[0]
+        return self.args
 
-    @property
-    def rhs(self):
-        return self.args[1]
+#    @property
+#    def rhs(self):
+#        return self.args[1]
 
     def match(self, e):
         # Go through and return an equivalent expression to e, or None
         # This uses self.lhs and self.rhs to find things == e
-        
-        
+        rule = list(self.lhs[0].args)
+        gate_num = len(rule)
+        if isinstance(e, Mul):
+            expr = list(e.args)
+            expr.reverse()
+            expr_len = len(expr)
+        else:
+            expr = [e]
+            expr_len = 1
+        if len(expr) > gate_num:
+            return None
+        else:
+            for c in range(gate_num):
+                rule.append(rule.pop(0))
+                rrule = rule[:]
+                rrule.reverse()
+                if rule[:expr_len] == expr:
+                    return rule[expr_len:]
+                elif rule[-expr_len:] == expr:
+                    return rule[:-expr_len]
+                elif rrule[:expr_len] == expr:
+                    return rrule[expr_len:]
+                elif rrule[-expr_len:] == expr:
+                    return rrule[:-expr_len]
+            return None
 
 _known_rules = []
-_known_rules.append(GateRule(Z(0), H(0)*X(0)*H(0)))
-_known_rules.append(GateRule(...))
-_known_rules.append(GateRule(...))
-_known_rules.append(GateRule(...))
-_known_rules.append(GateRule(...))
+_known_rules.append(GateRule(H(0)*X(0)*H(0)*Z(0)))
+_known_rules.append(GateRule(CNOT(1,0)*X(1)*CNOT(1,0)*X(1)*X(0)))
+_known_rules.append(GateRule(CNOT(1,0)*Y(1)*CNOT(1,0)*Y(1)*X(0)))
+_known_rules.append(GateRule(CNOT(1,0)*Y(0)*CNOT(1,0)*Y(0)*Z(1)))
+_known_rules.append(GateRule(CNOT(1,0)*Z(0)*CNOT(1,0)*Z(0)*Z(1)))
+_known_rules.append(GateRule(CNOT(1,0)*H(0)*H(1)*CNOT(0,1)*H(1)*H(0))
 
 def match_gate_rules(e):
     result = []
