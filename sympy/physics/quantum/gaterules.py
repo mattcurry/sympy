68a7c98f64ef5d69457d0a14021b69b9524eca3a
Matt Curry <mattjcurry@gmail.com>
2011-06-08 20:07:04 -0700

    Removed redundant rules.

--- sympy/physics/quantum/gaterules.py
+++ sympy/physics/quantum/gaterules.py
@@ -57,8 +57,6 @@ _known_rules.append(GateRule(CNOT(1,0)*Y(1)*CNOT(1,0)*Y(1)*X(0)))
 _known_rules.append(GateRule(CNOT(1,0)*Y(0)*CNOT(1,0)*Y(0)*Z(1)))
 _known_rules.append(GateRule(CNOT(1,0)*Z(0)*CNOT(1,0)*Z(0)*Z(1)))
 _known_rules.append(GateRule(CNOT(1,0)*H(0)*H(1)*CNOT(0,1)*H(1)*H(0)))
-_known_rules.append(GateRule(CNOT(1,0)*Z(0)*CNOT(1,0)*CNOT(0,1)*Z(1)*CNOT(0,1)))
-_known_rules.append(GateRule(CNOT(1,0)*X(1)*CNOT(1,0)*CNOT(0,1)*X(0)*CNOT(0,1)))
 _known_rules.append(GateRule(CNOT(1,0)*Y(1)*Z(0)*CNOT(1,0)*X(1)*Y(0)))
 
 def match_gate_rules(e):
